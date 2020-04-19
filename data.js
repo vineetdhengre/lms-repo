a07a1015d852de934047688117c0492e487aee66
DhengreAni18 <dhengreani18@gmail.com>
2020-04-19 15:32:50 +0530

    Setup socketio

--- data.js
+++ data.js
@@ -1,47 +1,50 @@
 const { MongoClient } = require("mongodb");
 const key = require("./mongo").mongoURI;
+const express = require("express");
+const mongoose = require("mongoose");
 
-// Replace the following with your Atlas connection string                                                                                                                                        
-
+const app = express();
+// Replace the following with your Atlas connection string
 
 const client = new MongoClient(key);
- 
- // The database to use
- const dbName = "test";
-                      
- module.exports = async function run() {
-    try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db(dbName);
 
-         // Use the collection "people"
-         const col = db.collection("people");
+// The database to use
+const dbName = "test";
 
-         // Construct a document                                                                                                                                                              
-         let personDocument = {
-             "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
-             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
-             "views": 1250000
-         }
+module.exports = async function run() {
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
 
-         // Insert a single document, wait for promise so we can read it back
-         const p = await col.insertOne(personDocument);
-         // Find one document
-         const myDoc = await col.findOne();
-         // Print to the console
-         console.log(myDoc);
+    // Use the collection "people"
+    const col = db.collection("people");
 
-        } catch (err) {
-         console.log(err.stack);
-     }
- 
-     finally {
-        await client.close();
-    }
-}
+    // Construct a document
+    //  let personDocument = {
+    //      "name": { "first": "Alan", "last": "Turing" },
+    //      "birth": new Date(1912, 5, 23), // June 23, 1912
+    //      "death": new Date(1954, 5, 7),  // June 7, 1954
+    //      "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+    //      "views": 1250000
+    //  }
+
+    //  // Insert a single document, wait for promise so we can read it back
+    //  const p = await col.insertOne(personDocument);
+    // Find one document
+    const myDoc = await col.findOne({
+      idd: "1",
+    });
+    //  document.getElementById("text").innerHTML = myDoc.name;
+
+    // Print to the console
+    console.log(myDoc.name);
+    app.get("/", (req, res) => res.send(myDoc.name));
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
 
 // run().catch(console.dir);
-
--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-
+var server = app.listen(8000);
+var io = require("socket.io").listen(server);
 const key = require("./mongo").mongoURI;
-const fun = require('./data');
+const fun = require("./data");
 
 mongoose
   .connect(key, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("DB Connected");
 
-    // fun();
+    fun();
   });
 const port = 8000;
 
-app.listen(port, () => {
-  console.log(`listening at port ${port}`);
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/data", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
