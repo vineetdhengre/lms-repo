c8d6ccfc7b61af58077b01b53f8eacb0cfb67058
DhengreAni18 <dhengreani18@gmail.com>
2020-04-20 11:25:17 +0530

    added socket emit

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const { MongoClient } = require("mongodb");
 const app = express();
 var server = app.listen(8000);
 var io = require("socket.io").listen(server);
 const key = require("./mongo").mongoURI;
-const fun = require("./data");
+const client = new MongoClient(key);
+
 
 mongoose
   .connect(key, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("DB Connected");
 
-    fun();
+    async function run() {
+      const dbName = "test";
+      try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "people"
+        const col = db.collection("people");
+
+        const myDoc = await col.findOne({
+          idd: "1",
+        });
+
+        console.log(myDoc.name);
+        io.on("connection", (socket) => {
+          console.log("data connected");
+          socket.emit("chat message", myDoc.name.first);
+        });
+        
+      } catch (err) {
+        console.log(err.stack);
+      } finally {
+        await client.close();
+      }
+    }
+
+    run().catch(console.dir);
   });
-const port = 8000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/data", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
-
-    
