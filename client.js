09fc124572023cdaa7a83da3d44d47af846e7a1c
DhengreAni18 <dhengreani18@gmail.com>
2020-04-23 12:47:49 +0530

    Changes

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://localhost:8000');
 socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+    document.getElementById('datauser').innerHTML = msg;
   });
